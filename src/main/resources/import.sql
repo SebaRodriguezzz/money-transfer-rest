f89a0466386ebc735caad47888663f57773b33ac
Seba Rodriguez <sebas.r1070@gmail.com>
2023-12-10 22:18:16 -0300

    Add file to insert entities to h2 database

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO bank_user (name, nationality, birth_date, balance) VALUES ('John Doe', 'USA', '1990-05-15', 1000.00);
+
+INSERT INTO bank_user (name, nationality, birth_date, balance) VALUES ('Alice Smith', 'Canada', '1985-12-28', 2500.50);
