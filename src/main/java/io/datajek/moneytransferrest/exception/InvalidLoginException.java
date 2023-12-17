f50f6c72921abed3e94b06758ade8845ac715462
SebaRodriguezzz <sebas.r1070@gmail.com>
2023-12-17 17:58:51 -0300

    Remove unnecessary comment

--- src/main/java/io/datajek/moneytransferrest/exception/InvalidLoginException.java
+++ src/main/java/io/datajek/moneytransferrest/exception/InvalidLoginException.java
@@ -1,6 +1,5 @@
 package io.datajek.moneytransferrest.exception;
 
-//TODO: diferencias exception y runtime, que es handler etc
 public class InvalidLoginException extends RuntimeException{
     public InvalidLoginException(String message) {
         super(message);
