09fd7998d82f00d844b3b38523298bd95b7ff395
SebaRodriguezzz <sebas.r1070@gmail.com>
2023-12-19 22:53:03 -0300

    Add transaction entity

---
+++ src/main/java/io/datajek/moneytransferrest/model/TransactionEntity.java
@@ -0,0 +1,72 @@
+package io.datajek.moneytransferrest.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Entity
+public class TransactionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private Instant date;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private UserEntity sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private UserEntity receiver;
+    private BigDecimal amount;
+
+    public TransactionEntity() {
+    }
+
+    public TransactionEntity(Instant date, UserEntity sender, UserEntity receiver, BigDecimal amount) {
+        this.date = date;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public UserEntity getSender() {
+        return sender;
+    }
+
+    public void setSender(UserEntity sender) {
+        this.sender = sender;
+    }
+
+    public UserEntity getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(UserEntity receiver) {
+        this.receiver = receiver;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
