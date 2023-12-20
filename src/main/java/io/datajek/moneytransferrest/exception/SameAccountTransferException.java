3aa865cf164469b9a55363e3061f48ed798d2ade
SebaRodriguezzz <sebas.r1070@gmail.com>
2023-12-19 22:53:59 -0300

    Correct names

--- src/main/java/io/datajek/moneytransferrest/exception/SameAccountTransferException.java
+++
@@ -1,7 +0,0 @@
-package io.datajek.moneytransferrest.exception;
-
-public class SameAccountTransferException extends RuntimeException {
-    public SameAccountTransferException(String message) {
-        super(message);
-    }
-}
