b6517c3f7fc99d383efe079d2201909ebde8e72b
SebaRodriguezzz <sebas.r1070@gmail.com>
2023-12-19 22:53:09 -0300

    Add transaction mapper

---
+++ src/main/java/io/datajek/moneytransferrest/service/mapper/TransactionMapper.java
@@ -0,0 +1,18 @@
+package io.datajek.moneytransferrest.service.mapper;
+
+import io.datajek.moneytransferrest.dto.TransactionDTO;
+import io.datajek.moneytransferrest.model.TransactionEntity;
+
+public class TransactionMapper {
+
+    public TransactionDTO toTransferDTO(TransactionEntity transactionEntity) {
+        TransactionDTO transactionDTO = new TransactionDTO();
+        transactionDTO.setId(transactionEntity.getId());
+        transactionDTO.setDate(transactionEntity.getDate());
+        transactionDTO.setSenderAccountNumber(transactionEntity.getSender().getAccountNumber());
+        transactionDTO.setReceiverAccountNumber(transactionEntity.getReceiver().getAccountNumber());
+        transactionDTO.setAmount(transactionEntity.getAmount());
+        return transactionDTO;
+    }
+
+}
