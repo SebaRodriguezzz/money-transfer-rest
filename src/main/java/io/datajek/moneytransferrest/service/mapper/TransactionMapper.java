e841b9ccb03bdaf2c39f4346f5d8dc38a604afbf
SebaRodriguezzz <sebas.r1070@gmail.com>
2024-02-05 00:43:16 -0300

    Add MapStruct

--- src/main/java/io/datajek/moneytransferrest/service/mapper/TransactionMapper.java
+++ src/main/java/io/datajek/moneytransferrest/service/mapper/TransactionMapper.java
@@ -2,30 +2,14 @@ package io.datajek.moneytransferrest.service.mapper;
 
 import io.datajek.moneytransferrest.dto.TransactionDTO;
 import io.datajek.moneytransferrest.model.TransactionEntity;
+import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Component
-public class TransactionMapper {
-
-    public TransactionDTO toTransactionDTO(TransactionEntity transactionEntity) {
-        TransactionDTO transactionDTO = new TransactionDTO();
-        transactionDTO.setId(transactionEntity.getId());
-        transactionDTO.setDate(transactionEntity.getDate());
-        transactionDTO.setSenderAccountNumber(transactionEntity.getSender().getAccountNumber());
-        transactionDTO.setReceiverAccountNumber(transactionEntity.getReceiver().getAccountNumber());
-        transactionDTO.setAmount(transactionEntity.getAmount());
-        return transactionDTO;
-    }
-
-    public List<TransactionDTO> toTransactionDTOList(List<TransactionEntity> transactionEntities) {
-        return transactionEntities.stream()
-                .map(this::toTransactionDTO)
-                .collect(Collectors.toList());
-    }
-
-
-
+@Mapper(componentModel = "spring")
+public interface TransactionMapper {
+    TransactionDTO toTransactionDTO(TransactionEntity transactionEntity);
+    List<TransactionDTO> toTransactionDTOList(List<TransactionEntity> transactionEntities);
 }
--- src/main/java/io/datajek/moneytransferrest/service/mapper/UserMapper.java
+++ src/main/java/io/datajek/moneytransferrest/service/mapper/UserMapper.java
@@ -2,39 +2,13 @@ package io.datajek.moneytransferrest.service.mapper;
 
 import io.datajek.moneytransferrest.dto.UserDTO;
 import io.datajek.moneytransferrest.model.UserEntity;
-import org.springframework.stereotype.Component;
-
+import org.mapstruct.Mapper;
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Component
-public class UserMapper {
 
-    public UserDTO toUserDTO(UserEntity userEntity) {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setId(userEntity.getId());
-        userDTO.setName(userEntity.getName());
-        userDTO.setNationality(userEntity.getNationality());
-        userDTO.setBirthDate(userEntity.getBirthDate());
-        userDTO.setAccountNumber(userEntity.getAccountNumber());
-        userDTO.setBalance(userEntity.getBalance());
-        return userDTO;
-    }
-
-    public UserEntity toUserEntity(UserDTO userDTO) {
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(userDTO.getId());
-        userEntity.setName(userDTO.getName());
-        userEntity.setNationality(userDTO.getNationality());
-        userEntity.setBirthDate(userDTO.getBirthDate());
-        userEntity.setAccountNumber(userDTO.getAccountNumber());
-        userEntity.setBalance(userDTO.getBalance());
-        return userEntity;
-    }
-
-    public List<UserDTO> toUserDTOList(List<UserEntity> userEntities) {
-        return userEntities.stream()
-                .map(this::toUserDTO)
-                .collect(Collectors.toList());
-    }
+@Mapper(componentModel = "spring")
+public interface  UserMapper {
+    UserDTO toUserDTO(UserEntity userEntity);
+    UserEntity toUserEntity(UserDTO userDTO);
+    List<UserDTO> toUserDTOList(List<UserEntity> userEntities);
 }
