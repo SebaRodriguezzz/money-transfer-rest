e52ede5c9a746718188d436a1f84146858b8f939
SebaRodriguezzz <sebas.r1070@gmail.com>
2024-02-05 00:43:51 -0300

    Improve code with lombok

--- src/main/java/io/datajek/moneytransferrest/controller/AuthenticationController.java
+++ src/main/java/io/datajek/moneytransferrest/controller/AuthenticationController.java
@@ -5,6 +5,7 @@ import io.datajek.moneytransferrest.dto.CredentialsDTO;
 import io.datajek.moneytransferrest.model.UserEntity;
 import io.datajek.moneytransferrest.service.AuthenticationService;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,14 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class AuthenticationController implements AuthenticationControllerAPI {
 
     private final AuthenticationService authenticationService;
-
-    @Autowired
-    public AuthenticationController(AuthenticationService authenticationService) {
-        this.authenticationService = authenticationService;
-    }
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody UserEntity user) {
--- src/main/java/io/datajek/moneytransferrest/controller/TransactionController.java
+++ src/main/java/io/datajek/moneytransferrest/controller/TransactionController.java
@@ -5,6 +5,8 @@ import io.datajek.moneytransferrest.dto.TransactionDTO;
 import io.datajek.moneytransferrest.model.TransactionEntity;
 import io.datajek.moneytransferrest.service.TransactionService;
 import io.datajek.moneytransferrest.service.mapper.TransactionMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,15 +14,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/transactions")
+@RequiredArgsConstructor
 public class TransactionController implements TransactionControllerAPI {
 
     private final TransactionService transactionService;
     private final TransactionMapper transactionMapper;
-
-    public TransactionController(TransactionService transactionService, TransactionMapper transactionMapper) {
-        this.transactionService = transactionService;
-        this.transactionMapper = transactionMapper;
-    }
 
     @GetMapping("/{id}")
     public ResponseEntity<TransactionDTO> findById(@PathVariable Long id) {
